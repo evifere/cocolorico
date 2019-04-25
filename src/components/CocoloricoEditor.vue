638edc6dabaca8199a68ffda073b4523150538dd
JOLLY <evifere@gmail.com>
2019-04-25 15:29:39 +0200

    change layout

--- src/components/CocoloricoEditor.vue
+++ src/components/CocoloricoEditor.vue
@@ -1,13 +1,6 @@
 <template>
   <el-container>
-    <el-aside width="508px" v-show="!isEditable">
-      <img width="500" height="700" ref="preview" src>
-    </el-aside>
-    <el-aside width="1225px" v-show="isEditable">
-      <canvas id="background" tabindex="0"></canvas>
-    </el-aside>
-    <el-container>
-      <el-header>Cocolorico !</el-header>
+    <el-header>
       <el-menu
         :default-active="activeIndex"
         class="el-menu-demo"
@@ -23,233 +16,246 @@
           </el-menu-item>
         </el-submenu>
       </el-menu>
+    </el-header>
+    <el-main>
+      <el-container>
+      <el-aside width="1225px" v-show="!isEditable">
+        <img width="1200" height="800" ref="preview" src>
+      </el-aside>
+      <el-aside width="1225px" v-show="isEditable">
+        <canvas id="background" tabindex="0"></canvas>
+      </el-aside>
       <el-main>
-        <hr>
-        <el-col class="params-panel">
-          <el-row>
-            <el-col :span="4" class="col-label col-text-center">
-              <label>Police</label>
-            </el-col>
-            <el-col :span="4">
-              <el-select
-                v-model="currentTextObjectConfig.fontFamily"
-                placeholder="Police"
-                v-bind:disabled="!isTextSelected || !isEditable"
-              >
-                <el-option v-for="font in allFonts" :key="font" :label="font" :value="font">
-                  <span :style="'font-family:'+font">{{ font }}</span>
-                </el-option>
-              </el-select>
-            </el-col>
-            <el-col :span="4" class="col-label col-text-center">
-              <label>Graisse</label>
-            </el-col>
-            <el-col :span="4">
-              <el-select
-                v-model="currentTextObjectConfig.fontWeight"
-                placeholder="font weight"
-                v-bind:disabled="!isTextSelected || !isEditable"
-              >
-                <el-option v-for="font in fontWeights" :key="font" :label="font" :value="font"></el-option>
-              </el-select>
-            </el-col>
-            <el-col :span="4" class="col-label col-text-center">
-              <label>Style</label>
-            </el-col>
-            <el-col :span="4">
-              <el-select
-                v-model="currentTextObjectConfig.fontStyle"
-                placeholder="font weight"
-                v-bind:disabled="!isTextSelected || !isEditable"
-              >
-                <el-option v-for="font in fontStyles" :key="font" :label="font" :value="font"></el-option>
-              </el-select>
-            </el-col>
-          </el-row>
-          <el-row type="flex" justify="center">
-            <el-col :span="10">
-              <el-checkbox v-model="currentTextObjectConfig.underline">Underline</el-checkbox>
-              <el-checkbox v-model="currentTextObjectConfig.linethrough">Line through</el-checkbox>
-              <el-checkbox v-model="currentTextObjectConfig.overline">Overline</el-checkbox>
-            </el-col>
-          </el-row>
-          <el-row class="empty"></el-row>
-          <el-row>
-            <el-col :span="4" class="col-label col-text-left">Ajouter bloc texte :</el-col>
-            <el-col :span="1">
-              <el-button
-                icon="el-icon-circle-plus"
-                type="success"
-                v-bind:disabled="!isEditable"
-                v-on:click="addTextBlock"
-                circle
-                size="small"
-              ></el-button>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8" class="col-label col-text-left">
-              <label>Couleur de fond</label>
-            </el-col>
-            <el-col :span="16">
-              <el-select
-                v-model="mainColor"
-                placeholder="Couleur de fond"
-                v-bind:disabled="!isEditable"
-              >
-                <el-option
-                  v-for="mainColor in mainColors"
-                  :key="mainColor"
-                  :label="mainColor"
-                  :value="mainColor"
-                >
-                  <el-row>
-                    <el-col :span="16">
-                      <span>{{ mainColor }}</span>
-                    </el-col>
-                    <el-col :span="8">
-                      <div class="color-box" :style="'background:'+mainColor+';'"></div>
-                    </el-col>
-                  </el-row>
-                </el-option>
-              </el-select>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8" class="col-label col-text-left">
-              <label>Couleur texte</label>
-            </el-col>
-            <el-col :span="16">
-              <el-select
-                v-model="currentTextObjectConfig.fill"
-                placeholder="La couleur du texte"
-                v-bind:disabled="!isTextSelected  || !isEditable"
-              >
-                <el-option
-                  v-for="fillColor in mainColors"
-                  :key="fillColor"
-                  :label="fillColor"
-                  :value="fillColor"
-                >
-                  <el-row>
-                    <el-col :span="16">
-                      <span>{{ fillColor }}</span>
-                    </el-col>
-                    <el-col :span="8">
-                      <div class="color-box" :style="'background:'+fillColor+';'"></div>
-                    </el-col>
-                  </el-row>
-                </el-option>
-              </el-select>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8" class="col-label col-text-left">
-              <label>Text align</label>
-            </el-col>
-            <el-col :span="16">
-              <el-select
-                v-model="currentTextObjectConfig.textAlign"
-                placeholder="Alignement"
-                v-bind:disabled="!isTextSelected  || !isEditable"
-              >
-                <el-option
-                  v-for="alignment in alignments"
-                  :key="alignment"
-                  :label="alignment"
-                  :value="alignment.toLowerCase()"
-                ></el-option>
-              </el-select>
-            </el-col>
-          </el-row>
-          <el-row class="empty"></el-row>
-          <el-row>
-            <el-col :span="4" class="col-label col-text-left">
-              <label>Thunder Stroke</label>
-            </el-col>
-            <el-col :span="4">
-              <el-color-picker
-                v-model="currentTextObjectConfig.stroke"
-                size="mini"
-                color-format="hex"
-              ></el-color-picker>
-            </el-col>
-            <el-col :span="16">
-              <el-slider
-                v-model="currentTextObjectConfig.strokeWidth"
-                :min="0"
-                :max="10"
-                :step="0.1"
-                show-input
-                v-bind:disabled="!isTextSelected || !isEditable"
-              ></el-slider>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8" class="col-label col-text-left">
-              <label>Font size</label>
-            </el-col>
-            <el-col :span="16">
-              <el-slider
-                v-model="currentTextObjectConfig.fontSize"
-                :min="1"
-                :max="120"
-                :step="1"
-                show-input
-                v-bind:disabled="!isTextSelected  || !isEditable"
-              ></el-slider>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-col :span="8" class="col-label col-text-left">
-              <label>Line height</label>
-            </el-col>
-            <el-col :span="16">
-              <el-slider
-                v-model="currentTextObjectConfig.lineHeight"
-                :min="0"
-                :max="10"
-                :step="0.1"
-                show-input
-                v-bind:disabled="!isTextSelected  || !isEditable"
-              ></el-slider>
-            </el-col>
-          </el-row>
+        <el-header>Cocolorico !</el-header>
 
-          <el-row type="flex" justify="center">
-            <el-col :span="8">
-              <el-button
-                type="primary"
-                v-bind:disabled="!isEditable"
-                v-on:click="moveForward"
-              >avancer</el-button>
-            </el-col>
-            <el-col :span="4" class="col-label col-text-center">et</el-col>
-            <el-col :span="8">
-              <el-button
-                type="primary"
-                v-bind:disabled="!isEditable"
-                v-on:click="moveBackward"
-              >reculer</el-button>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-switch
-              v-model="isEditable"
-              active-text="Edition en cours"
-              inactive-text="Mode Preview"
-              v-on:change="saveToPng"
-            ></el-switch>
-          </el-row>
-          <hr>
-          <el-row>
-            <a ref="downloadPng" href="#" download="cocoricover.png" v-show="false"></a>
-            <el-button type="primary" v-on:click="downloadPng" icon="el-icon-download">Png</el-button>
-          </el-row>
-        </el-col>
+        <el-container>
+          <el-col class="params-panel">
+            <el-row>
+              <el-col :span="4" class="col-label col-text-center">
+                <label>Police</label>
+              </el-col>
+              <el-col :span="4">
+                <el-select
+                  v-model="currentTextObjectConfig.fontFamily"
+                  placeholder="Police"
+                  v-bind:disabled="!isTextSelected || !isEditable"
+                >
+                  <el-option v-for="font in allFonts" :key="font" :label="font" :value="font">
+                    <span :style="'font-family:'+font">{{ font }}</span>
+                  </el-option>
+                </el-select>
+              </el-col>
+              <el-col :span="4" class="col-label col-text-center">
+                <label>Graisse</label>
+              </el-col>
+              <el-col :span="4">
+                <el-select
+                  v-model="currentTextObjectConfig.fontWeight"
+                  placeholder="font weight"
+                  v-bind:disabled="!isTextSelected || !isEditable"
+                >
+                  <el-option v-for="font in fontWeights" :key="font" :label="font" :value="font"></el-option>
+                </el-select>
+              </el-col>
+              <el-col :span="4" class="col-label col-text-center">
+                <label>Style</label>
+              </el-col>
+              <el-col :span="4">
+                <el-select
+                  v-model="currentTextObjectConfig.fontStyle"
+                  placeholder="font weight"
+                  v-bind:disabled="!isTextSelected || !isEditable"
+                >
+                  <el-option v-for="font in fontStyles" :key="font" :label="font" :value="font"></el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <el-row type="flex" justify="center">
+              <el-col :span="10">
+                <el-checkbox v-model="currentTextObjectConfig.underline">Underline</el-checkbox>
+                <el-checkbox v-model="currentTextObjectConfig.linethrough">Line through</el-checkbox>
+                <el-checkbox v-model="currentTextObjectConfig.overline">Overline</el-checkbox>
+              </el-col>
+            </el-row>
+            <el-row class="empty"></el-row>
+            <el-row>
+              <el-col :span="4" class="col-label col-text-left">Ajouter bloc texte :</el-col>
+              <el-col :span="1">
+                <el-button
+                  icon="el-icon-circle-plus"
+                  type="success"
+                  v-bind:disabled="!isEditable"
+                  v-on:click="addTextBlock"
+                  circle
+                  size="small"
+                ></el-button>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="8" class="col-label col-text-left">
+                <label>Couleur de fond</label>
+              </el-col>
+              <el-col :span="16">
+                <el-select
+                  v-model="mainColor"
+                  placeholder="Couleur de fond"
+                  v-bind:disabled="!isEditable"
+                >
+                  <el-option
+                    v-for="mainColor in mainColors"
+                    :key="mainColor"
+                    :label="mainColor"
+                    :value="mainColor"
+                  >
+                    <el-row>
+                      <el-col :span="16">
+                        <span>{{ mainColor }}</span>
+                      </el-col>
+                      <el-col :span="8">
+                        <div class="color-box" :style="'background:'+mainColor+';'"></div>
+                      </el-col>
+                    </el-row>
+                  </el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="8" class="col-label col-text-left">
+                <label>Couleur texte</label>
+              </el-col>
+              <el-col :span="16">
+                <el-select
+                  v-model="currentTextObjectConfig.fill"
+                  placeholder="La couleur du texte"
+                  v-bind:disabled="!isTextSelected  || !isEditable"
+                >
+                  <el-option
+                    v-for="fillColor in mainColors"
+                    :key="fillColor"
+                    :label="fillColor"
+                    :value="fillColor"
+                  >
+                    <el-row>
+                      <el-col :span="16">
+                        <span>{{ fillColor }}</span>
+                      </el-col>
+                      <el-col :span="8">
+                        <div class="color-box" :style="'background:'+fillColor+';'"></div>
+                      </el-col>
+                    </el-row>
+                  </el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="8" class="col-label col-text-left">
+                <label>Text align</label>
+              </el-col>
+              <el-col :span="16">
+                <el-select
+                  v-model="currentTextObjectConfig.textAlign"
+                  placeholder="Alignement"
+                  v-bind:disabled="!isTextSelected  || !isEditable"
+                >
+                  <el-option
+                    v-for="alignment in alignments"
+                    :key="alignment"
+                    :label="alignment"
+                    :value="alignment.toLowerCase()"
+                  ></el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <el-row class="empty"></el-row>
+            <el-row>
+              <el-col :span="4" class="col-label col-text-left">
+                <label>Thunder Stroke</label>
+              </el-col>
+              <el-col :span="4">
+                <el-color-picker
+                  v-model="currentTextObjectConfig.stroke"
+                  size="mini"
+                  color-format="hex"
+                ></el-color-picker>
+              </el-col>
+              <el-col :span="16">
+                <el-slider
+                  v-model="currentTextObjectConfig.strokeWidth"
+                  :min="0"
+                  :max="10"
+                  :step="0.1"
+                  show-input
+                  v-bind:disabled="!isTextSelected || !isEditable"
+                ></el-slider>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="8" class="col-label col-text-left">
+                <label>Font size</label>
+              </el-col>
+              <el-col :span="16">
+                <el-slider
+                  v-model="currentTextObjectConfig.fontSize"
+                  :min="1"
+                  :max="120"
+                  :step="1"
+                  show-input
+                  v-bind:disabled="!isTextSelected  || !isEditable"
+                ></el-slider>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="8" class="col-label col-text-left">
+                <label>Line height</label>
+              </el-col>
+              <el-col :span="16">
+                <el-slider
+                  v-model="currentTextObjectConfig.lineHeight"
+                  :min="0"
+                  :max="10"
+                  :step="0.1"
+                  show-input
+                  v-bind:disabled="!isTextSelected  || !isEditable"
+                ></el-slider>
+              </el-col>
+            </el-row>
+
+            <el-row type="flex" justify="center">
+              <el-col :span="8">
+                <el-button
+                  type="primary"
+                  v-bind:disabled="!isEditable"
+                  v-on:click="moveForward"
+                >avancer</el-button>
+              </el-col>
+              <el-col :span="4" class="col-label col-text-center">et</el-col>
+              <el-col :span="8">
+                <el-button
+                  type="primary"
+                  v-bind:disabled="!isEditable"
+                  v-on:click="moveBackward"
+                >reculer</el-button>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-switch
+                v-model="isEditable"
+                active-text="Edition en cours"
+                inactive-text="Mode Preview"
+                v-on:change="saveToPng"
+              ></el-switch>
+            </el-row>
+            <hr>
+            <el-row>
+              <a ref="downloadPng" href="#" download="cocoricover.png" v-show="false"></a>
+              <el-button type="primary" v-on:click="downloadPng" icon="el-icon-download">Télécharger</el-button>
+            </el-row>
+          </el-col>
+        </el-container>
       </el-main>
-      <el-footer align="right">copyright 2018-2019 @cocolorico v{{version}}</el-footer>
-    </el-container>
+      </el-container>
+    </el-main>
+    <el-footer align="right">copyright 2018-2019 @cocolorico v{{version}}</el-footer>
   </el-container>
 </template>
 
